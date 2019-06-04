65bd13cef0e6c285ceb13e82f5fc465ae5b1b22b
MaximilianKoegl <maximilian.koegl@stud.uni-regensburg.de>
2019-06-04 04:39:12 -0500

    Added CSV-log for last written word in sentence. Removed logging for empty sentence. Made more pep8 conform

--- text_entry_speed_test.py
+++ text_entry_speed_test.py
@@ -8,12 +8,14 @@ import csv
 import io
 # from table_logger import TableLogger
 
+
 class SuperText(QtWidgets.QTextEdit):
- 
+
     def __init__(self):
         super(SuperText, self).__init__()
 
-        # method to print logs to console in a nice formatted tabular way. easier to read, but not valid csv. disabled by default
+        # method to print logs to console in a nice formatted tabular way.
+        # easier to read, but not valid csv. disabled by default
         # self.tbl = TableLogger(columns="timestamp,event,input,time")
 
         self.word_timer = QtCore.QTime()
@@ -27,8 +29,8 @@ class SuperText(QtWidgets.QTextEdit):
         self.textChanged.connect(self.changedText)
 
         self.initUI()
-        
-    def initUI(self):      
+
+    def initUI(self):
         self.setGeometry(0, 0, 400, 400)
         self.setWindowTitle("SuperText")
         self.setFocusPolicy(QtCore.Qt.StrongFocus)
@@ -37,11 +39,11 @@ class SuperText(QtWidgets.QTextEdit):
 
     def changedText(self):
         self.handleTimer()
-        # Delete last char and prevent measurement from errors 
+        # Delete last char and prevent measurement from errors
         if not len(self.prev_content) > len(self.toPlainText()):
             self.handleText()
         self.prev_content = self.toPlainText()
-    
+
     def handleText(self):
         last_char = self.toPlainText()[-1:]
 
@@ -52,7 +54,12 @@ class SuperText(QtWidgets.QTextEdit):
 
             # self.tbl(self.timestamp(), "word typed", self.current_word, self.stop_measurement(self.word_timer))
 
-            self.log_csv([self.timestamp(), "word typed", self.current_word, self.stop_measurement(self.word_timer)])
+            self.log_csv([
+                            self.timestamp(),
+                            "word typed",
+                            self.current_word,
+                            self.stop_measurement(self.word_timer)
+                        ])
             self.current_word = ""
 
         elif last_char == "\n":
@@ -60,19 +67,30 @@ class SuperText(QtWidgets.QTextEdit):
             self.sentence += self.current_word
 
             # self.tbl(self.timestamp(), "sentence typed", self.sentence, self.stop_measurement(self.sentence_timer))
-
-            self.log_csv([self.timestamp(), "sentence typed", self.sentence, self.stop_measurement(self.sentence_timer)])
+            if self.current_word != "":
+                self.log_csv([
+                                self.timestamp(),
+                                "word typed",
+                                self.current_word,
+                                self.stop_measurement(self.word_timer)
+                            ])
+            if self.sentence != "":
+                self.log_csv([
+                                self.timestamp(),
+                                "sentence typed",
+                                self.sentence,
+                                self.stop_measurement(self.sentence_timer)
+                            ])
             self.sentence = ""
             self.current_word = ""
-        
+
         # Add latest character to word
         else:
             self.current_word += last_char
-            
+
             # self.tbl(self.timestamp(), "character typed", last_char, 0)
 
             self.log_csv([self.timestamp(), "character typed", last_char, 0])
-        
 
     def handleTimer(self):
         if not self.is_running_word:
@@ -92,13 +110,14 @@ class SuperText(QtWidgets.QTextEdit):
     def timestamp(self):
         return QtCore.QDateTime.currentDateTime().toString(QtCore.Qt.ISODate)
 
-    # convert log-data from list to csv-string and output it to stdout 
+    # convert log-data from list to csv-string and output it to stdout
     # https://stackoverflow.com/questions/9157314/how-do-i-write-data-into-csv-format-as-string-not-file
     def log_csv(self, data):
         si = io.StringIO()
         cw = csv.writer(si)
         cw.writerow(data)
         print(si.getvalue().strip("\r\n"))
+
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
