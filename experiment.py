c7ef63816bff93e8d560f39b5df01c9bb58ea4cb
MaximilianKoegl <maximilian.koegl@stud.uni-regensburg.de>
2019-06-04 08:52:26 -0500

    Setup files

--- experiment.py
+++ experiment.py
@@ -3,14 +3,18 @@
 
 import sys
 from PyQt5 import QtGui, QtCore, QtWidgets
-import csv
+import json
 from text_entry_speed_test import SuperText
 # from text_input_technique import SuperText
 
+
 class TextEditExperiment(QtWidgets.QWidget):
 
-    def __init__(self):
+    def __init__(self, user_id, method, sentences):
         super(TextEditExperiment, self).__init__()
+        self.user_id = user_id
+        self.method = method
+        self.sentences = sentences
         self.initUI()
 
     def initUI(self):
@@ -23,11 +27,23 @@ class TextEditExperiment(QtWidgets.QWidget):
         self.layout.addWidget(SuperText())
         self.setLayout(self.layout)
         self.show()
+        print(self.sentences)
+
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
-    super_text = TextEditExperiment()
+    super_text = TextEditExperiment(*parse_setup(sys.argv[1]))
     sys.exit(app.exec_())
+
+
+def parse_setup(filename):
+    with open(filename) as f:
+        contents = json.load(f)
+    user_id = contents['user_id']
+    method = contents['method']
+    sentences = contents['sentences']
+    return user_id, method, sentences
+
 
 if __name__ == '__main__':
     main()
